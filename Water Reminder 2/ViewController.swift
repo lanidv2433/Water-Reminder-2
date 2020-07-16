06e53ed69e4dac7ad08660c37efae8813648bac9
lanidv2433 <lanidv2433@gmail.com>
2020-07-16 13:15:57 -0400

    Initial Commit

---
+++ Water Reminder 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Water Reminder 2
+//
+//  Created by Lan-Anh Dang-Vu on 7/16/20.
+//  Copyright © 2020 Lan-Anh Dang-Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
